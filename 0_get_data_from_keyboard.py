53b99356ff28bde6e118392f5ecf2da30806c33e
minced-garlic <59724724+minced-garlic@users.noreply.github.com>
2021-09-09 15:59:18 +0900

    Update 0_get_data_from_keyboard.py

--- 0_get_data_from_keyboard.py
+++ 0_get_data_from_keyboard.py
@@ -1,12 +1,15 @@
-import keyboard
 import os
 from pynput.keyboard import Key, Listener
 import numpy as np
+import glob
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
 class get_data_from_key:
     def __init__(self):
         self.keys = set()
         self.line = np.zeros(14, dtype=np.int32)
         self.on = False
+        self.dir_pp= './raw data/'
         with Listener(
                 on_press=self.on_press,
                 on_release=self.on_release
@@ -20,11 +23,14 @@ class get_data_from_key:
             self.data_file_txt = "./raw data/data_" + str('{0:03d}'.format(i))+'.npy'
             print('start the save!')
             self.line = np.zeros(14)
-            np.save(self.data_file_txt,np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'u', 'd', 'r', 'l']))
+            np.save(self.data_file_txt,[])
             self.on=True
 
     def end(self):
-        if self.on: self.on=False
+        if self.on:
+            self.on=False
+            self.data_preprocessing(self.dir_pp)
+            print('end the save!')
 
     def on_press(self, key):
         if self.on:
@@ -54,6 +60,13 @@ class get_data_from_key:
             elif key == Key.right:          self.line[13] = 0
             elif key.char in '0':           self.line[9] = 0
             elif key.char in '123456789':   self.line[int(key.char) - 1] = 0
+
+    def data_preprocessing(dir_pp):
+        print("start of preprocessing.")
+        flist = sorted(glob.glob(dir_pp + '*'))
+        X_train = [np.load(fname) for fname in flist]
+        X_max_len = np.max([len(X_line) for X_line in X_train])
+        np.save('./X_train.npy', pad_sequences(X_train, maxlen=X_max_len, padding='post'))
 
 
 if __name__ == '__main__':
